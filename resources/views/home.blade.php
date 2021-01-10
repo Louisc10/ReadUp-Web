7c02c7492a8c5c00566fd81af82dac9d688df40d
Louis Choules <42336657+Louisc10@users.noreply.github.com>
2021-01-10 22:28:39 +0700

    Updating File Naming

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
             <h5 class="uppercase ">the best source for</h5>
             <div class="scroller">
                 <h2 class="uppercase scrolling">Manga</h2>
-                <h2 class="uppercase scrolling">Novel</h2>
+                <h2 class="uppercase scrolling">Comic</h2>
             </div>
             <h5 class="uppercase">free, without any ads and in high definition!</h5>
             <h1 class="material-icons" id="go-down">expand_more</h1>
@@ -22,10 +22,10 @@
     </div>
     <div class="hs1 container">
         <h3><a href="/hot">Popular Now</a></h3>
-        @include('layout.displayAllFilm', ['books' => $popular, 'sect' => 'popular'])
+        @include('layout.displayAllBook', ['books' => $popular, 'sect' => 'popular'])
     </div>
     <div class="hs1 container">
         <h3>Latest Upload</h3>
-        @include('layout.displayAllFilm', ['books' => $latest, 'sect' => 'latest'])
+        @include('layout.displayAllBook', ['books' => $latest, 'sect' => 'latest'])
     </div>
 @endsection
--- resources/views/hot.blade.php
+++ resources/views/hot.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="hs1 container">
         <h3>Hot Manga</h3>
-        @include('layout.displayAllFilm', ['books' => $hots])
+        @include('layout.displayAllBook', ['books' => $hots])
     </div>
     <div class="justify-content-md-center paginator pg-1">{{$hots->onEachSide(3)->links()}}</div>
 @endsection
